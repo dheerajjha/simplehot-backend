2d92ba6313dbac1acd35a63a403f9080f61b6675
Ubuntu <azureuser@analytics.ce5d2rho2naehp0pyi5ha0tmng.rx.internal.cloudapp.net>
2025-06-01 17:46:54 +0000

    Fix DB creation script to make it better

--- postgres/init-db.sh
+++ postgres/init-db.sh
@@ -4,26 +4,32 @@ set -e
 set -u
 
 echo "Creating databases for SimpleHot microservices..."
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE auth_db;
-    GRANT ALL PRIVILEGES ON DATABASE auth_db TO $POSTGRES_USER;
-    
-    CREATE DATABASE user_db;
-    GRANT ALL PRIVILEGES ON DATABASE user_db TO $POSTGRES_USER;
-    
-    CREATE DATABASE post_db;
-    GRANT ALL PRIVILEGES ON DATABASE post_db TO $POSTGRES_USER;
-    
-    CREATE DATABASE stock_db;
-    GRANT ALL PRIVILEGES ON DATABASE stock_db TO $POSTGRES_USER;
-    
-    CREATE DATABASE prediction_db;
-    GRANT ALL PRIVILEGES ON DATABASE prediction_db TO $POSTGRES_USER;
-EOSQL
+
+# Function to create database if it doesn't exist
+create_database_if_not_exists() {
+    local db_name=$1
+    echo "Checking if database $db_name exists..."
+    if ! psql -U "$POSTGRES_USER" -lqt | cut -d \| -f 1 | grep -qw "$db_name"; then
+        echo "Creating database $db_name..."
+        psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE $db_name;"
+    else
+        echo "Database $db_name already exists, skipping creation."
+    fi
+    echo "Granting privileges on $db_name..."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "GRANT ALL PRIVILEGES ON DATABASE $db_name TO $POSTGRES_USER;"
+}
+
+# Create all databases
+create_database_if_not_exists "auth_db"
+create_database_if_not_exists "user_db"
+create_database_if_not_exists "post_db"
+create_database_if_not_exists "stock_db"
+create_database_if_not_exists "prediction_db"
 
 # Auth Service tables
+echo "Setting up auth_db tables..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth_db" <<-EOSQL
-    CREATE TABLE "User" (
+    CREATE TABLE IF NOT EXISTS "User" (
         "id" SERIAL NOT NULL,
         "email" TEXT NOT NULL,
         "password" TEXT NOT NULL,
@@ -35,13 +41,14 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "auth_db" <<-EOSQL
         "updatedAt" TIMESTAMP(3) NOT NULL,
         CONSTRAINT "User_pkey" PRIMARY KEY ("id")
     );
-    CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-    CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+    CREATE UNIQUE INDEX IF NOT EXISTS "User_email_key" ON "User"("email");
+    CREATE UNIQUE INDEX IF NOT EXISTS "User_username_key" ON "User"("username");
 EOSQL
 
 # Post Service tables
+echo "Setting up post_db tables..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "post_db" <<-EOSQL
-    CREATE TABLE "Post" (
+    CREATE TABLE IF NOT EXISTS "Post" (
         "id" SERIAL NOT NULL,
         "content" TEXT NOT NULL,
         "imageUrl" TEXT,
@@ -51,7 +58,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "post_db" <<-EOSQL
         CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "Like" (
+    CREATE TABLE IF NOT EXISTS "Like" (
         "id" SERIAL NOT NULL,
         "userId" INTEGER NOT NULL,
         "postId" INTEGER NOT NULL,
@@ -59,7 +66,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "post_db" <<-EOSQL
         CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "Comment" (
+    CREATE TABLE IF NOT EXISTS "Comment" (
         "id" SERIAL NOT NULL,
         "content" TEXT NOT NULL,
         "userId" INTEGER NOT NULL,
@@ -69,18 +76,28 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "post_db" <<-EOSQL
         CONSTRAINT "Comment_pkey" PRIMARY KEY ("id")
     );
     
-    ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" 
-        FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+    -- Add foreign key constraints only if they don't exist
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'Like_postId_fkey') THEN
+            ALTER TABLE "Like" ADD CONSTRAINT "Like_postId_fkey" 
+                FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+        END IF;
+        
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'Comment_postId_fkey') THEN
+            ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" 
+                FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+        END IF;
+    END
+    \$\$;
     
-    ALTER TABLE "Comment" ADD CONSTRAINT "Comment_postId_fkey" 
-        FOREIGN KEY ("postId") REFERENCES "Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-    
-    CREATE UNIQUE INDEX "Like_userId_postId_key" ON "Like"("userId", "postId");
+    CREATE UNIQUE INDEX IF NOT EXISTS "Like_userId_postId_key" ON "Like"("userId", "postId");
 EOSQL
 
 # Stock Service tables
+echo "Setting up stock_db tables..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "stock_db" <<-EOSQL
-    CREATE TABLE "Stock" (
+    CREATE TABLE IF NOT EXISTS "Stock" (
         "id" SERIAL NOT NULL,
         "symbol" TEXT NOT NULL,
         "name" TEXT NOT NULL,
@@ -97,7 +114,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "stock_db" <<-EOSQL
         CONSTRAINT "Stock_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "StockHistory" (
+    CREATE TABLE IF NOT EXISTS "StockHistory" (
         "id" SERIAL NOT NULL,
         "stockSymbol" TEXT NOT NULL,
         "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -109,20 +126,27 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "stock_db" <<-EOSQL
         CONSTRAINT "StockHistory_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE UNIQUE INDEX "Stock_symbol_key" ON "Stock"("symbol");
-    CREATE UNIQUE INDEX "StockHistory_stockSymbol_date_key" ON "StockHistory"("stockSymbol", "date");
+    CREATE UNIQUE INDEX IF NOT EXISTS "Stock_symbol_key" ON "Stock"("symbol");
+    CREATE UNIQUE INDEX IF NOT EXISTS "StockHistory_stockSymbol_date_key" ON "StockHistory"("stockSymbol", "date");
     
-    -- Insert some sample stock data
+    -- Insert sample stock data only if table is empty
     INSERT INTO "Stock" ("symbol", "name", "currentPrice", "change", "changePercent", "volume", "marketCap", "dayHigh", "dayLow", "yearHigh", "yearLow")
-    VALUES 
-        ('RELIANCE', 'Reliance Industries', 2500.75, 25.50, 1.03, 3500000, 1500000000000, 2520.00, 2480.00, 2800.00, 2000.00),
-        ('TCS', 'Tata Consultancy Services', 3400.25, 45.30, 1.35, 2800000, 1200000000000, 3450.00, 3380.00, 3600.00, 3100.00),
-        ('INFY', 'Infosys', 1500.50, -12.25, -0.81, 2100000, 950000000000, 1520.00, 1490.00, 1800.00, 1400.00);
+    SELECT 'RELIANCE', 'Reliance Industries', 2500.75, 25.50, 1.03, 3500000, 1500000000000, 2520.00, 2480.00, 2800.00, 2000.00
+    WHERE NOT EXISTS (SELECT 1 FROM "Stock" WHERE symbol = 'RELIANCE');
+    
+    INSERT INTO "Stock" ("symbol", "name", "currentPrice", "change", "changePercent", "volume", "marketCap", "dayHigh", "dayLow", "yearHigh", "yearLow")
+    SELECT 'TCS', 'Tata Consultancy Services', 3400.25, 45.30, 1.35, 2800000, 1200000000000, 3450.00, 3380.00, 3600.00, 3100.00
+    WHERE NOT EXISTS (SELECT 1 FROM "Stock" WHERE symbol = 'TCS');
+    
+    INSERT INTO "Stock" ("symbol", "name", "currentPrice", "change", "changePercent", "volume", "marketCap", "dayHigh", "dayLow", "yearHigh", "yearLow")
+    SELECT 'INFY', 'Infosys', 1500.50, -12.25, -0.81, 2100000, 950000000000, 1520.00, 1490.00, 1800.00, 1400.00
+    WHERE NOT EXISTS (SELECT 1 FROM "Stock" WHERE symbol = 'INFY');
 EOSQL
 
 # Prediction Service tables
+echo "Setting up prediction_db tables..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "prediction_db" <<-EOSQL
-    CREATE TABLE "Prediction" (
+    CREATE TABLE IF NOT EXISTS "Prediction" (
         "id" SERIAL NOT NULL,
         "stockSymbol" TEXT NOT NULL,
         "stockName" TEXT NOT NULL,
@@ -139,7 +163,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "prediction_db" <<-
         CONSTRAINT "Prediction_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "PredictionLike" (
+    CREATE TABLE IF NOT EXISTS "PredictionLike" (
         "id" SERIAL NOT NULL,
         "predictionId" INTEGER NOT NULL,
         "userId" INTEGER NOT NULL,
@@ -147,7 +171,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "prediction_db" <<-
         CONSTRAINT "PredictionLike_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "PredictionComment" (
+    CREATE TABLE IF NOT EXISTS "PredictionComment" (
         "id" SERIAL NOT NULL,
         "content" TEXT NOT NULL,
         "predictionId" INTEGER NOT NULL,
@@ -157,18 +181,28 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "prediction_db" <<-
         CONSTRAINT "PredictionComment_pkey" PRIMARY KEY ("id")
     );
     
-    ALTER TABLE "PredictionLike" ADD CONSTRAINT "PredictionLike_predictionId_fkey" 
-        FOREIGN KEY ("predictionId") REFERENCES "Prediction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+    -- Add foreign key constraints only if they don't exist
+    DO \$\$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'PredictionLike_predictionId_fkey') THEN
+            ALTER TABLE "PredictionLike" ADD CONSTRAINT "PredictionLike_predictionId_fkey" 
+                FOREIGN KEY ("predictionId") REFERENCES "Prediction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+        END IF;
+        
+        IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'PredictionComment_predictionId_fkey') THEN
+            ALTER TABLE "PredictionComment" ADD CONSTRAINT "PredictionComment_predictionId_fkey" 
+                FOREIGN KEY ("predictionId") REFERENCES "Prediction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+        END IF;
+    END
+    \$\$;
     
-    ALTER TABLE "PredictionComment" ADD CONSTRAINT "PredictionComment_predictionId_fkey" 
-        FOREIGN KEY ("predictionId") REFERENCES "Prediction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-    
-    CREATE UNIQUE INDEX "PredictionLike_userId_predictionId_key" ON "PredictionLike"("userId", "predictionId");
+    CREATE UNIQUE INDEX IF NOT EXISTS "PredictionLike_userId_predictionId_key" ON "PredictionLike"("userId", "predictionId");
 EOSQL
 
 # User Service tables
+echo "Setting up user_db tables..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "user_db" <<-EOSQL
-    CREATE TABLE "User" (
+    CREATE TABLE IF NOT EXISTS "User" (
         "id" SERIAL NOT NULL,
         "email" TEXT NOT NULL,
         "name" TEXT,
@@ -180,7 +214,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "user_db" <<-EOSQL
         CONSTRAINT "User_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE TABLE "Follow" (
+    CREATE TABLE IF NOT EXISTS "Follow" (
         "id" SERIAL NOT NULL,
         "followerId" INTEGER NOT NULL,
         "followingId" INTEGER NOT NULL,
@@ -188,9 +222,9 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "user_db" <<-EOSQL
         CONSTRAINT "Follow_pkey" PRIMARY KEY ("id")
     );
     
-    CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-    CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
-    CREATE UNIQUE INDEX "Follow_followerId_followingId_key" ON "Follow"("followerId", "followingId");
+    CREATE UNIQUE INDEX IF NOT EXISTS "User_email_key" ON "User"("email");
+    CREATE UNIQUE INDEX IF NOT EXISTS "User_username_key" ON "User"("username");
+    CREATE UNIQUE INDEX IF NOT EXISTS "Follow_followerId_followingId_key" ON "Follow"("followerId", "followingId");
 EOSQL
 
-echo "All databases and tables have been created successfully" 
+echo "All databases and tables have been created successfully"
